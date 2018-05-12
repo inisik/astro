d415443d1e58fad91f844f040e1ace4ec834bffc
inisik <inisik0820@gmail.com>
2018-05-11 22:06:59 -0400

    Add the class that is the main object that controls everything.

---
+++ SocialAuthentication/Astro.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+/**
+ * Astro.
+ *
+ * A lightweight social authentication library designed to securely collect information from social networks.
+ *
+ * @author Astro Contributors <https://github.com/inisik/astro/graphs/contributors>
+ *
+ * @license <https://github.com/inisik/astro/blob/master/LICENSE>.
+ * @link    <https://github.com/inisik/astro>.
+ */
+
+namespace Astro\SocialAuthentication;
+
+/**
+ * @class RequestHandler.
+ */
+class Astro
+{
+
+    /**
+     * Open a new authentiction object to preform api request.
+     */
+    public function __construct(RequestHandler $requestHandler)
+    {
+    }
+}
