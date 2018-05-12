f31574f21252622c050d1dc01f53af7504317843
inisik <inisik0820@gmail.com>
2018-05-11 20:15:57 -0400

    Change version id.

---
+++ Astro.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Astro.
+ *
+ * A lightweight social authentication library designed to securely collect information from social networks.
+ *
+ * @author Astro Contributors <https://github.com/inisik/astro/graphs/contributors>
+ *
+ * @license <https://github.com/inisik/astro/blob/master/LICENSE>.
+ * @link    <https://github.com/inisik/astro>.
+ */
+
+define('ASTRO_VERSION_ID', 001000000);
+define('ASTRO_VERSION', 'v1.0.0');
+
