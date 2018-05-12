596660231061cc459ebafefe1ee19204931cf3c6
inisik <inisik0820@gmail.com>
2018-05-11 23:02:08 -0400

    Add the providers class.

---
+++ SocialAuthentication/Providers.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * Astro.
+ *
+ * A lightweight social authentication library designed to securely collect information from social networks.
+ *
+ * @author Astro Contributors <https://github.com/inisik/astro/graphs/contributors>
+ *
+ * @license <https://github.com/inisik/astro/blob/master/LICENSE>.
+ * @link    <https://github.com/inisik/astro>.
+ */
+
+namespace Astro\SocialAuthentication;
+
+/**
+ * @class Providers.
+ */
+class Providers
+{
+}
