83f925891d13d0aae73e20656d0f60f57c1fd1a7
inisik <inisik0820@gmail.com>
2018-05-11 22:19:06 -0400

    Make the method public.

--- SocialAuthentication/RequestHandler.php
+++ SocialAuthentication/RequestHandler.php
@@ -66,7 +66,7 @@ class RequestHandler implements RequestHandlerInterface
      *
      * @return bool Return true if the method is valid and false if othrwise.
      */
-    protected static function isValidMethod(string $method): bool
+    public static function isValidMethod(string $method): bool
     {
         return \in_array($method, $this->methods, \true);
     }    
