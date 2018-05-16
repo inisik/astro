75cdec8a8d6dac57ac42ef3dc5e6337778df86bb
inisik <inisik0820@gmail.com>
2018-05-16 16:41:31 -0400

    Add an interface to define each exception.

---
+++ SocialAuthentication/Exception/ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Astro.
+ *
+ * An authentication library designed to decrease securely risk and make login systems easy to deploy.
+ *
+ * @author Astro Contributors <https://github.com/inisik/astro/graphs/contributors>
+ *
+ * @license <https://github.com/inisik/astro/blob/master/LICENSE>.
+ * @link    <https://github.com/inisik/astro>.
+ */
+
+namespace Astro\Exception;
+
+/**
+ * @interface ExceptionInterface.
+ * @extends   Exception.
+ */
+class ExceptionInterface extends Exception
+{
+}
